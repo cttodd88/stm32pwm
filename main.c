7c913c3a2a09428cd20a154139ff3f4f87e262e5
elex <elex@elex-Veriton-M680G>
2015-04-27 17:07:53 -0700

    Display code now working with existing PWM code. Removed some commented out sections of code that are not needed for readability.

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 // Author: Chris Todd
 // ID: C0304228
 // Creation date: 3/10/2015
-// Last updated:
+// Last updated: 4/27/2015 - Taylor Long
+// Description: Integrated Display code with PWM code
 
 #include "cmsis_boot/stm32f4xx.h"
 #include "cmsis_lib/include/stm32f4xx_gpio.h"
@@ -31,11 +32,9 @@ GPIO_InitTypeDef  GPIO_InitStructure;
 NVIC_InitTypeDef nvicStructure;
 
 /* Private define ------------------------------------------------------------*/
-#define GREEN  GPIOD, GPIO_Pin_12
-#define ORANGE GPIOD, GPIO_Pin_13
-#define RED GPIOD, GPIO_Pin_14
-#define BLUE GPIOD, GPIO_Pin_15
+
 /* Private macro -------------------------------------------------------------*/
+
 /* Private variables ---------------------------------------------------------*/
 unsigned int TimingDelay=0;
 TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
@@ -57,38 +56,33 @@ extern GUI_PID_STATE pstate;
 
 int duty =0;
 int index2 = 0;
-int period=0;
-
-//char *title = "PQ PWM C.T. T.L. A.C. F.F.";
-
-int lookup[1601]={0};
-
-
+int lookup[101]={0};
 
 int main(void)
 {
+    /*Variables*/
+    int period = 13999;
+    
     /*Initialization*/
-    PROGBAR_Handle hProgbar;
+    //PROGBAR_Handle hProgbar;
+    BUTTON_Handle hButton;
     
     SystemInit();
         
     if(SysTick_Config(SystemCoreClock/1000)){
         while(1);
     }
-              
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
-    //RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);                
+    
+    Init_GPIO(); // Ports C,D,E Clocks enabled in here
+    //RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 
-    period=874;
+    //period=874;
+    genLookup(lookup,100,13999);
 
-	genLookup(lookup,1600,874);
-
-	 /* Alternating functions for pins */
-	//PWM_TIMER_Init(220);  //40Khz 2083
-	PWM_TIMER_Init3(period); //500 KHz 166
-	//PWM_TIMER_Init8(8399);
+    /* Alternating functions for pins */
+    //PWM_TIMER_Init(220);  //40Khz 2083
+    PWM_TIMER_Init3(period); //500 KHz 166
+    //PWM_TIMER_Init8(8399);
     /*
 	PWM_InitOC1(83);
     PWM_InitOC2(4199);
@@ -96,47 +90,27 @@ int main(void)
     PWM_InitOC4(83);
 	*/
     //GPIO_PinAFConfig(GPIOD, GPIO_PinSource12, GPIO_AF_TIM4);
-   // GPIO_PinAFConfig(GPIOD, GPIO_PinSource13, GPIO_AF_TIM4);
-   // GPIO_PinAFConfig(GPIOD, GPIO_PinSource14, GPIO_AF_TIM4);
+    //GPIO_PinAFConfig(GPIOD, GPIO_PinSource13, GPIO_AF_TIM4);
+    //GPIO_PinAFConfig(GPIOD, GPIO_PinSource14, GPIO_AF_TIM4);
     //GPIO_PinAFConfig(GPIOD, GPIO_PinSource15, GPIO_AF_TIM4);
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource8, GPIO_AF_TIM3);
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource9, GPIO_AF_TIM3);
+    GPIO_PinAFConfig(GPIOC, GPIO_PinSource8, GPIO_AF_TIM3);
+    GPIO_PinAFConfig(GPIOC, GPIO_PinSource9, GPIO_AF_TIM3);
     //GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_TIM1);
-    /* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
 
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8|GPIO_Pin_9;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-      GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8|GPIO_Pin_9;
-   	  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-   	  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-   	  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-   	  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-   	  GPIO_Init(GPIOC, &GPIO_InitStructure);
+    TIM_ITConfig(TIM3,TIM_IT_Update,ENABLE);
+    nvicStructure.NVIC_IRQChannel = TIM3_IRQn;
+    nvicStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    nvicStructure.NVIC_IRQChannelSubPriority = 1;
+    nvicStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&nvicStructure);
 
-	 /* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
-
-	  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15;
-	  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-	  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	  GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-	  //Configure LCD Peripherals
-	  GPIO_InitStructure.GPIO_Pin = (GPIO_Pin_8|GPIO_Pin_9|GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
-	  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	  GPIO_Init(GPIOE, &GPIO_InitStructure);
-
-
-	  TIM_ITConfig(TIM3,TIM_IT_Update,ENABLE);
-	  nvicStructure.NVIC_IRQChannel = TIM3_IRQn;
-	  nvicStructure.NVIC_IRQChannelPreemptionPriority = 0;
-	  nvicStructure.NVIC_IRQChannelSubPriority = 1;
-	  nvicStructure.NVIC_IRQChannelCmd = ENABLE;
-	  NVIC_Init(&nvicStructure);
-
-    Init_GPIO();
     Init_FSMC();
     
     /* Enable the CRC Module */
@@ -152,27 +126,31 @@ int main(void)
     //turn on lcd backlight
     GPIO_ResetBits(GPIOD, GPIO_Pin_12);
 
-    rtcConfig();
+    //rtcConfig();
 
     GUI_SetFont(&GUI_Font8x16);
     GUI_SetBkColor(GUI_BLUE);
     GUI_Clear();
-    GUI_DispString("Elex268 Lab6 - RTC");
+    GUI_DispString("Elex290 Sentinel Power");
     //  GUI_DispString(GUI_GetVersionString());
     GUI_DispString("\n\n\n");
-    GUI_DrawGradientH(5, 150, 315, 235, 0x0000FF, 0x00FFFF);
+    //GUI_DrawGradientH(5, 150, 315, 235, 0x0000FF, 0x00FFFF);
 
-    PROGBAR_SetDefaultSkin(PROGBAR_SKIN_FLEX); // Sets the default skin for new widgets
+    BUTTON_SetDefaultSkin(BUTTON_SKIN_FLEX); // Sets the default skin for new widgets
 
     /* Create progress bar at location x = 10, y = 10, length = 219, height = 30 */
-    hProgbar = PROGBAR_CreateEx(50, 180, 219, 30, 0, WM_CF_SHOW, 0, GUI_ID_PROGBAR0);
+    //hProgbar = PROGBAR_CreateEx(50, 180, 219, 30, 0, WM_CF_SHOW, 0, GUI_ID_PROGBAR0);
+    hButton = BUTTON_CreateEx(50, 180, 150, 50, 0, WM_CF_SHOW, 0, GUI_ID_BUTTON0);
+    BUTTON_SetFocussable(hButton, 1);
+    BUTTON_SetText(hButton, "Test");
+    
     /* Set progress bar font */
-    PROGBAR_SetFont(hProgbar, &GUI_Font8x16);
+    //PROGBAR_SetFont(hProgbar, &GUI_Font8x16);
 
-    PROGBAR_SetMinMax(hProgbar, 0, 59);
+    //PROGBAR_SetMinMax(hProgbar, 0, 59);
 
     /* Set progress bar text */
-    PROGBAR_SetText(hProgbar, "...");
+    //PROGBAR_SetText(hProgbar, "...");
     /*char *string;
 
     string="PQ PWM C.T. T.L. A.C. F.F.";
@@ -183,15 +161,22 @@ int main(void)
 
     while(1)
     {
-        int tempSeconds;
-	char tempSecString[5];
+        //int tempSeconds;
+	//char tempSecString[5];
 
-        printTime();
+        //printTime();
 
         GUI_PID_GetCurrentState(&pstate);
 
         GUI_SetFont(&GUI_Font8x16);
-
+        
+        if (BUTTON_IsPressed(hButton)){
+            BUTTON_SetText(hButton, "Pressed");
+        }
+        else {
+            BUTTON_SetText(hButton, "Test");
+        }
+        
         if (pstate.Pressed) {
             GUI_DispDecAt( pstate.x, 280,20,4);
             GUI_DispDecAt( pstate.y, 280,40,4);
@@ -204,12 +189,12 @@ int main(void)
         }
 
         //print progress bar displaying seconds
-        tempSeconds=myclockTimeStruct.RTC_Seconds;	//RTC_Seconds in BCD
-        tempSeconds=tempSeconds/16*10+tempSeconds%16;	//convert to decimal
-        sprintf(tempSecString, "%02d", tempSeconds);	//convert dec value to string
+        //tempSeconds=myclockTimeStruct.RTC_Seconds;	//RTC_Seconds in BCD
+        //tempSeconds=tempSeconds/16*10+tempSeconds%16;	//convert to decimal
+        //sprintf(tempSecString, "%02d", tempSeconds);	//convert dec value to string
 
-        PROGBAR_SetValue(hProgbar, tempSeconds);		//use dec value for bar position
-        PROGBAR_SetText(hProgbar, tempSecString);		//use string for value on progress bar
+        //PROGBAR_SetValue(hProgbar, tempSeconds);		//use dec value for bar position
+       // PROGBAR_SetText(hProgbar, tempSecString);		//use string for value on progress bar
 
         GUI_Delay(50);
 /*
@@ -299,27 +284,7 @@ int main(void)
     	}
 
     	Delay(250000);
-*/
-   /*GPIO_SetBits(GREEN);
-    	 Delay(250);
-    	 GPIO_ResetBits(GREEN);
-    	 Delay(250);
-
-    	 GPIO_SetBits(ORANGE);
-    	 Delay(250);
-    	 GPIO_ResetBits(ORANGE);
-    	 Delay(250);
-
-    	 GPIO_SetBits(RED);
-    	 Delay(250);
-    	 GPIO_ResetBits(RED);
-    	 Delay(250);
-
-    	 GPIO_SetBits(BLUE);
-    	 Delay(250);
-    	 GPIO_ResetBits(BLUE);
-    	 Delay(250); */     
-        
+*/        
     }//END WHILE
 
 
@@ -327,9 +292,6 @@ int main(void)
 
 
 }
-
-
-
 
 
 void Delay(unsigned int nTime)
@@ -358,7 +320,6 @@ void SysTick_Handler(void) {
 	TimingDelay_Decrement();
 }
 */
-
 void TIM3_IRQHandler(void){
 
 
@@ -368,13 +329,13 @@ void TIM3_IRQHandler(void){
 	        TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
 
 
-	if(index2<=800){
+	if(index2<=50){
 
 	PWMTIM3_InitOC4(lookup[index2]);
 
 	}
 
-	if(index2>=800){
+	if(index2>=50){
 
 	PWMTIM3_InitOC3(lookup[index2]);
 
@@ -384,7 +345,7 @@ void TIM3_IRQHandler(void){
 
 	index2++;
 
-	if(index2==1601){
+	if(index2==101){
 
 		index2=0;
 	}
@@ -418,6 +379,3 @@ void TIM3_IRQHandler(void){
 
   }
 }
-
-
-
