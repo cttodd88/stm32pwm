848cfdf46b63d23758264d1324d814e5fe4c4111
Taylor Long <karthias@gmail.com>
2015-05-13 14:16:28 -0700

    Initial branch commit2 - added build/ to commit before merge

--- build/Debug/GNU-Linux-x86/main.o.d
+++ build/Debug/GNU-Linux-x86/main.o.d
@@ -4,8 +4,35 @@ build/Debug/GNU-Linux-x86/main.o: main.c cmsis_boot/stm32f4xx.h \
  cmsis_boot/system_stm32f4xx.h cmsis_boot/stm32f4xx_conf.h \
  cmsis_lib/include/stm32f4xx_gpio.h \
  cmsis_lib/include/../../cmsis_boot/stm32f4xx.h \
+<<<<<<< HEAD
  cmsis_lib/include/stm32f4xx_rcc.h cmsis_lib/include/stm32f4xx_tim.h \
- cmsis_lib/include/stm32f4xx_pwm.h cmsis_lib/include/misc.h lcd/lcd.h
+ cmsis_lib/include/stm32f4xx_pwm.h cmsis_lib/include/misc.h inc/GUI.h \
+ inc/GUI_ConfDefaults.h inc/../Config/GUIConf.h inc/GUI_Type.h inc/LCD.h \
+ inc/Global.h inc/GUI_Version.h inc/DIALOG.h inc/WM.h inc/WM_GUI.h \
+ inc/GUI.h inc/BUTTON.h inc/DIALOG_Intern.h inc/WIDGET.h inc/WM_Intern.h \
+ inc/GUI_Private.h inc/LCD_Protected.h inc/GUI_Debug.h inc/SCROLLBAR.h \
+ inc/CALENDAR.h inc/CHECKBOX.h inc/CHOOSECOLOR.h inc/CHOOSEFILE.h \
+ inc/DROPDOWN.h inc/LISTBOX.h inc/EDIT.h inc/FRAMEWIN.h inc/GRAPH.h \
+ inc/HEADER.h inc/ICONVIEW.h inc/IMAGE.h inc/LISTVIEW.h inc/LISTWHEEL.h \
+ inc/MENU.h inc/MULTIEDIT.h inc/MULTIPAGE.h inc/DIALOG.h inc/PROGBAR.h \
+ inc/RADIO.h inc/SLIDER.h inc/SPINBOX.h inc/TEXT.h inc/TREEVIEW.h \
+ inc/KNOB.h inc/GUITDRV_ADS7846.h user/LCD_STM32F4.h \
+ user/../cmsis_boot/stm32f4xx.h user/stm32f4xx_it.h user/stm32f4xx_it.h \
+ user/pwm.h user/../cmsis_boot/stm32f4xx_conf.h user/ts.h \
+ user/../cmsis_lib/include/misc.h \
+ user/../cmsis_lib/include/stm32f4xx_spi.h \
+ user/../cmsis_lib/include/../../cmsis_boot/stm32f4xx.h \
+ user/../cmsis_lib/include/stm32f4xx_exti.h \
+ user/../cmsis_lib/include/stm32f4xx_gpio.h user/LCD_STM32F4.h \
+ user/stm32f4xx_conf.h user/../inc/GUI.h delays.h rtc.h \
+ cmsis_lib/include/stm32f4xx_rtc.h cmsis_lib/include/stm32f4xx_pwr.h \
+ img/sentinelLogo2.h img/../inc/GUI.h img/sentinelLogoBW.h
+=======
+ cmsis_lib/include/stm32f4xx_rcc.h cmsis_lib/include/stm32f4xx_dma.h \
+ cmsis_lib/include/stm32f4xx_tim.h cmsis_lib/include/stm32f4xx_pwm.h \
+ cmsis_lib/include/misc.h cmsis_lib/include/stm32f4xx_adc.h lcd/lcd.h \
+ sensor_init.h
+>>>>>>> origin/PWM_dev
 
 cmsis_boot/stm32f4xx.h:
 
@@ -27,10 +54,152 @@ cmsis_lib/include/../../cmsis_boot/stm32f4xx.h:
 
 cmsis_lib/include/stm32f4xx_rcc.h:
 
+cmsis_lib/include/stm32f4xx_dma.h:
+
 cmsis_lib/include/stm32f4xx_tim.h:
 
 cmsis_lib/include/stm32f4xx_pwm.h:
 
 cmsis_lib/include/misc.h:
 
+<<<<<<< HEAD
+inc/GUI.h:
+
+inc/GUI_ConfDefaults.h:
+
+inc/../Config/GUIConf.h:
+
+inc/GUI_Type.h:
+
+inc/LCD.h:
+
+inc/Global.h:
+
+inc/GUI_Version.h:
+
+inc/DIALOG.h:
+
+inc/WM.h:
+
+inc/WM_GUI.h:
+
+inc/GUI.h:
+
+inc/BUTTON.h:
+
+inc/DIALOG_Intern.h:
+
+inc/WIDGET.h:
+
+inc/WM_Intern.h:
+
+inc/GUI_Private.h:
+
+inc/LCD_Protected.h:
+
+inc/GUI_Debug.h:
+
+inc/SCROLLBAR.h:
+
+inc/CALENDAR.h:
+
+inc/CHECKBOX.h:
+
+inc/CHOOSECOLOR.h:
+
+inc/CHOOSEFILE.h:
+
+inc/DROPDOWN.h:
+
+inc/LISTBOX.h:
+
+inc/EDIT.h:
+
+inc/FRAMEWIN.h:
+
+inc/GRAPH.h:
+
+inc/HEADER.h:
+
+inc/ICONVIEW.h:
+
+inc/IMAGE.h:
+
+inc/LISTVIEW.h:
+
+inc/LISTWHEEL.h:
+
+inc/MENU.h:
+
+inc/MULTIEDIT.h:
+
+inc/MULTIPAGE.h:
+
+inc/DIALOG.h:
+
+inc/PROGBAR.h:
+
+inc/RADIO.h:
+
+inc/SLIDER.h:
+
+inc/SPINBOX.h:
+
+inc/TEXT.h:
+
+inc/TREEVIEW.h:
+
+inc/KNOB.h:
+
+inc/GUITDRV_ADS7846.h:
+
+user/LCD_STM32F4.h:
+
+user/../cmsis_boot/stm32f4xx.h:
+
+user/stm32f4xx_it.h:
+
+user/stm32f4xx_it.h:
+
+user/pwm.h:
+
+user/../cmsis_boot/stm32f4xx_conf.h:
+
+user/ts.h:
+
+user/../cmsis_lib/include/misc.h:
+
+user/../cmsis_lib/include/stm32f4xx_spi.h:
+
+user/../cmsis_lib/include/../../cmsis_boot/stm32f4xx.h:
+
+user/../cmsis_lib/include/stm32f4xx_exti.h:
+
+user/../cmsis_lib/include/stm32f4xx_gpio.h:
+
+user/LCD_STM32F4.h:
+
+user/stm32f4xx_conf.h:
+
+user/../inc/GUI.h:
+
+delays.h:
+
+rtc.h:
+
+cmsis_lib/include/stm32f4xx_rtc.h:
+
+cmsis_lib/include/stm32f4xx_pwr.h:
+
+img/sentinelLogo2.h:
+
+img/../inc/GUI.h:
+
+img/sentinelLogoBW.h:
+=======
+cmsis_lib/include/stm32f4xx_adc.h:
+
 lcd/lcd.h:
+
+sensor_init.h:
+>>>>>>> origin/PWM_dev
